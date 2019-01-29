7eeefadc2a3e5cceb93fe2cf3583d55f3ce79e50
tripthilohit <tripthilohit@gmail.com>
2019-01-28 22:22:50 -0500

    exercise

--- tests/note.js
+++ tests/note.js
@@ -1,23 +1,2 @@
-//retuen the largest word in a sentence
+//nth largest number
 
-var lengthLargestWord = function(str)
-{
-
-var strnew = [] 
-strnew = str.split(" ")
-var largestLength = strnew[0].length
-for(var i=0;i<strnew.length;i++)
-{
-	
-	if(strnew[i].length>largestLength)
-		largestLength = strnew[i].length
-		
-}
-
-for(var j=0;j<strnew.length;j++)
-{
-if(strnew[j].length === largestLength)
-console.log(strnew[j])
-}
-}
-lengthLargestWord("hello how are you???")
